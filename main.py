1c6e3b639c6afcb27aa514d163e6eda9d7fa4b50
Benhariga Abderrahmane <benhariga.abderrahmane@gmail.com>
2023-01-06 14:57:43 +0100

    edit

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from werkzeug.utils import secure_filename
 import os
 from wtforms.validators import InputRequired, DataRequired
 import flask # Import the module
+from datetime import datetime
 
 print(flask.__version__)
 
@@ -165,7 +166,7 @@ for user in users:
 
 class UploadFileForm(FlaskForm):
     file = FileField("File", validators=[InputRequired()])
-    submit = SubmitField("Upload File")
+    submit = SubmitField("Upload")
 
 
 ########### end controllers ###########
@@ -181,13 +182,13 @@ def index():
     books = booksdoc.getElementsByTagName('book')
     bookObjectCollection.clear()
     for book in books:
-            types = book.getElementsByTagName('type')
-            i = 0
-            alltypes.clear()
-            for _ in types:
-                alltypes.append(types[i].childNodes[0].nodeValue)
-                i = i+1
-            bookObjectCollection.append(Book(book.getElementsByTagName('title')[0].firstChild.data, book.getElementsByTagName('author')[0].firstChild.data, book.getElementsByTagName('publisher')[0].firstChild.data, book.getElementsByTagName('date')[0].firstChild.data, alltypes,
+        types = book.getElementsByTagName('type')
+        i = 0
+        alltypes.clear()
+        for _ in types:
+            alltypes.append(types[i].childNodes[0].nodeValue)
+            i = i+1
+        bookObjectCollection.append(Book(book.getElementsByTagName('title')[0].firstChild.data, book.getElementsByTagName('author')[0].firstChild.data, book.getElementsByTagName('publisher')[0].firstChild.data, book.getElementsByTagName('date')[0].firstChild.data, alltypes,
                                     book.getElementsByTagName('description')[0].firstChild.data, book.attributes['ISBN'].value, book.attributes['price'].value, book.attributes['cover'].value))
     return render_template('index.html', bookObjectCollection=bookObjectCollection)
 
@@ -197,6 +198,47 @@ def index():
 @ app.route('/signup')
 def signup():
     return render_template('signup.html')
+
+
+# Booklist
+
+
+
+@ app.route('/booklist', methods=['POST', 'GET'])
+def booklist():
+
+    booksdoc = minidom.parse('xmldata/books.xml')
+    books = booksdoc.getElementsByTagName('book')
+    bookObjectCollection.clear()
+    for book in books:
+        types = book.getElementsByTagName('type')
+        i = 0
+        alltypes.clear()
+        for _ in types:
+            alltypes.append(types[i].childNodes[0].nodeValue)
+            i = i+1
+        bookObjectCollection.append(Book(book.getElementsByTagName('title')[0].firstChild.data, book.getElementsByTagName('author')[0].firstChild.data, book.getElementsByTagName('publisher')[0].firstChild.data, book.getElementsByTagName('date')[0].firstChild.data, alltypes,
+                                    book.getElementsByTagName('description')[0].firstChild.data, book.attributes['ISBN'].value, book.attributes['price'].value, book.attributes['cover'].value))
+    
+    if request.method == "POST":
+        ISBN = request.form['ISBN']
+        if request.form['btn'] == 'Delete':
+            books = booksdoc2.getroot()
+
+            for book in books:
+                if book.attrib['ISBN'] == ISBN:
+                    book.getparent().remove(book)
+
+            bookstr = minidom.parseString(etree.tostring(books, pretty_print=True, encoding='unicode')).toprettyxml()
+            bookxml = minidom.parseString(bookstr)
+        
+            with open("xmldata/books.xml", "w", encoding="utf-8") as xml_file:
+                bookxml.writexml(xml_file)
+            
+            return redirect(url_for('booklist'))
+        
+    return render_template('booklist.html' , books=bookObjectCollection)
+
 
 # login
 
@@ -217,12 +259,13 @@ def login():
 # Product
 
 
+
 @app.route('/<isbn>', methods=['POST', 'GET'])
 def show_product(isbn):
     for book in bookObjectCollection:
         if (isbn == book.ISBN):
             return render_template('product.html', book=book)
-    return render_template('base.html')
+    return render_template('404.html')
 
 # Logout
 
@@ -232,6 +275,79 @@ def logout():
     g.user = None
     session.pop('user_id', None)
     return redirect(url_for('index'))
+
+# Editbook
+
+
+@ app.route('/<isbn>/editbook', methods=['GET',"POST"])
+def editbook(isbn):
+    form = UploadFileForm()
+    if request.method == "POST":
+        ISBN = request.form['isbn']
+        price = request.form['price']
+        cover = "/static/img/" + request.form['isbn']
+        type = request.form['typey']
+        title = request.form['title']
+        author = request.form['author']
+        publisher = request.form['publisher']
+        date = request.form['date']
+        description = request.form['description']
+        # create elements
+        Bookk = etree.Element("book", ISBN=ISBN, price=price, cover=cover)
+        Title = etree.Element("title")
+        Author = etree.Element("author")
+        Publisher = etree.Element("publisher")
+        Date = etree.Element("date")
+        Types = etree.Element("types")
+        Description = etree.Element("description")
+        # add value
+        Title.text = title
+        Author.text = author
+        Publisher.text = publisher
+        Date.text = date
+        textt = ''
+        for i in type:
+            print(textt)
+            if i == ',':
+                Type = etree.Element("type")
+                Type.text = textt
+                Types.append(Type)
+                textt = ''
+            else:
+                textt = textt + i
+        Type = etree.Element("type")
+        Type.text = textt
+        Types.append(Type)
+        textt = ''
+        Description.text = description 
+        Types.append(Type)
+        Bookk.append(Title)
+        Bookk.append(Author)
+        Bookk.append(Publisher)
+        Bookk.append(Date)
+        Bookk.append(Types)
+        Bookk.append(Description)
+        # Delete the old book
+        root = booksdoc2.getroot()
+        for book in root:
+            if book.attrib['ISBN'] == isbn:
+                book.getparent().remove(book)
+        
+        root.append(Bookk)
+        bookstr = minidom.parseString(etree.tostring(root, pretty_print=True, encoding='unicode')).toprettyxml()
+        bookxml = minidom.parseString(bookstr)
+        
+        with open("xmldata/books.xml", "w", encoding="utf-8") as xml_file:
+            bookxml.writexml(xml_file)
+        
+        if form.validate_on_submit():
+            file = form.file.data # First grab the file
+            file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(request.form['isbn']  + ".png"))) # Then save the file
+    
+    for book in bookObjectCollection:
+        if (isbn == book.ISBN):
+            return render_template('editbook.html', book=book, form=form)
+    return render_template('404.html')
 
 
 # Addbook
@@ -256,7 +372,6 @@ def addbook():
         Author = etree.Element("author")
         Publisher = etree.Element("publisher")
         Date = etree.Element("date")
-        Type = etree.Element("type")
         Types = etree.Element("types")
         Description = etree.Element("description")
         # add value
@@ -264,8 +379,20 @@ def addbook():
         Author.text = author
         Publisher.text = publisher
         Date.text = date
-        Type.text = type
+        textt = ''
+        for i in type:
+            print(textt)
+            if i == ',':
+                Type = etree.Element("type")
+                Type.text = textt
+                Types.append(Type)
+                textt = ''
+            else:
+                textt = textt + i
+        Type = etree.Element("type")
+        Type.text = textt
         Types.append(Type)
+        textt = ''
         Description.text = description 
         Bookk.append(Title)
         Bookk.append(Author)
@@ -275,10 +402,11 @@ def addbook():
         Bookk.append(Description)
         root = booksdoc2.getroot()
         root.append(Bookk)
-        bookstr = minidom.parseString(etree.tostring(root))
+        bookstr = minidom.parseString(etree.tostring(root, pretty_print=True, encoding='unicode')).toprettyxml()
+        bookxml = minidom.parseString(bookstr)
         
         with open("xmldata/books.xml", "w", encoding="utf-8") as xml_file:
-            bookstr.writexml(xml_file)
+            bookxml.writexml(xml_file)
         
         if form.validate_on_submit():
             file = form.file.data # First grab the file
@@ -289,4 +417,4 @@ def addbook():
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
